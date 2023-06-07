6344f0d3b7754ff05228158f9c0228fd9c3d9192
qweds ytr <shantanuwverma@gmail.com>
2023-06-07 00:34:05 -0700

    test

---
+++ src/components/Todo.jsx
@@ -0,0 +1,6 @@
+import TodoItems from "./TodoItems";
+export default function Todo(){
+    return (
+        <TodoItems note={"hello"}/>
+    );
+}
---
+++ src/components/TodoItems.jsx
@@ -0,0 +1,3 @@
+export default function({note}){
+    return <p>{note}</p>;
+}
