f8b4f2fe3af8db725434c8a665ca6721127162ab
qweds ytr <shantanuwverma@gmail.com>
2023-06-07 03:03:24 -0700

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,10 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 
 import vercel from "@astrojs/vercel/serverless";
-
+import image from '@astrojs/image';
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(),image()],
   output: "server",
   adapter: vercel()
 });
