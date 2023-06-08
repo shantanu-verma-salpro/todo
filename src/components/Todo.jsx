f4a8ad51ee931aabd23984c7bdeb3bb67f1fe5ba
shantanu-verma-salpro <shantanuwverma@gmail.com>
2023-06-08 14:36:26 +0530

    test

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,21 +1,68 @@
-import React, { useState } from "react";
-import TodoItems from "./TodoItems";
-import AddTodo from "./AddTodo";
+import React, { useRef, useState } from "react";
 
 const Todo = () => {
-  const [todoList, setTodoList] = useState(["hello", "hi"]);
+    const [tasks, setTasks] = useState([]);
+    const inputRefs = useRef({
+        title: null,
+        description: null,
+        priority: null
+    });
 
-  const addTodo = (task) => {
-    setTodoList((prevTodoList) => [...prevTodoList, task]);
-  };
+    const handleTaskSubmit = () => {
+        const title = inputRefs.current.title.value.trim();
+        const description = inputRefs.current.description.value.trim();
+        const priority = inputRefs.current.priority.value.trim();
 
-  return (
-    <>
-      <h1>Todo List</h1>
-      <AddTodo addTodo={addTodo} />
-      <TodoItems note={todoList} />
-    </>
-  );
+        if (title && description && priority) {
+            const newTask = {
+                title,
+                description,
+                priority,
+                timestamp: new Date().toLocaleDateString(),
+                isCompleted: false
+            };
+
+            setTasks((prevTasks) => [...prevTasks, newTask]);
+
+            // Reset input values
+            inputRefs.current.title.value = "";
+            inputRefs.current.description.value = "";
+            inputRefs.current.priority.value = "";
+        }
+    };
+
+    const renderTaskInputBox = () => (
+        <>
+            <label htmlFor="title">Title</label>
+            <input ref={inputRefs.current.title} id="title" type="text" />
+            <label htmlFor="desc">Description</label>
+            <input ref={inputRefs.current.description} id="desc" type="text" />
+            <label htmlFor="pr">Priority</label>
+            <input ref={inputRefs.current.priority} id="pr" type="number" />
+        </>
+    );
+
+    const renderSubmitButton = () => (
+        <button onClick={handleTaskSubmit}>Add Task</button>
+    );
+
+    const renderTaskPanel = () =>
+        tasks.map((task) => (
+            <div key={task.timestamp}>
+                <div>{task.title}</div>
+                <div>{task.description}</div>
+                <div>{task.priority}</div>
+                <div>{task.timestamp}</div>
+            </div>
+        ));
+
+    return (
+        <>
+            {renderTaskInputBox()}
+            {renderSubmitButton()}
+            {renderTaskPanel()}
+        </>
+    );
 };
 
 export default Todo;
