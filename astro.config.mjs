89885588a8310f7d54beb408402edda102866e10
shantanu-verma-salpro <shantanuwverma@gmail.com>
2023-06-09 22:16:49 +0530

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-import { astroImageTools } from "astro-imagetools";
 import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(),astroImageTools],
+  integrations: [react()],
   output: "server",
   adapter: vercel()
 });
